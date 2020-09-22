a8accf6cd9fb44c51037579279ba67b40f97fda4
zahra.mahmood98 <zahra.mahmood98@hotmail.co.uk>
2020-09-22 22:06:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,5 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if(string === "I love you, Grandma."){
     return("I love you, too.")
-  } else if(string.toLowerCase === string){
-      return("I can't hear you!")
   }
 }
