56f473e3464ef59968bb97d69bf85772e3630a63
zahra.mahmood98 <zahra.mahmood98@hotmail.co.uk>
2020-09-22 21:48:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 function shout(string) {
   return string.toUpperCase()
 }
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.whisper())
+}
+
+function sayHiToGrandma(string) {
+  switch(string) {
+      case string === whisper(string) || log.whisper(string):
+        return "I can't hear you!"
+        break;
+      case string === shout(string) || log.shout(string):
+        return "YES INDEED!"
+        break;
+      case string === "I love you, Grandma.":
+        return "Ilove you, too."
+        break;
+  }
+}
+
