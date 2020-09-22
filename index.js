e340cc1ad492457abde2f2f0f61993a041e8503c
zahra.mahmood98 <zahra.mahmood98@hotmail.co.uk>
2020-09-22 22:09:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,4 +18,10 @@ function sayHiToGrandma(string) {
   if(string === "I love you, Grandma."){
     return("I love you, too.")
   }
+  if(string === string.toLowerCase()){
+    return("I can't hear you!")
+  }
+  if(string === string.toUpperCase()){
+    return("YES INDEED!")
+  }
 }
